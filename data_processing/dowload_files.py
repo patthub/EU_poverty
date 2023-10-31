b5dd69eb35ca416ca706d36fa0626b65f98341e9
Emilia Wiśnios <wisniosemilia@gmail.com>
2023-10-31 16:45:02 +0100

    bug fix

--- data_processing/dowload_files.py
+++ data_processing/dowload_files.py
@@ -3,6 +3,7 @@ import os
 from loguru import logger
 import joblib
 import urllib
+from time import sleep
 
 proxies = {
   "http": "",
@@ -23,35 +24,67 @@ def get_links_to_download(links_dir: str) -> list:
 
 
 def download_file(file_url: str, saving_dir: str) -> None:
+    max_retries = 5
+    retries = 0
     file_name_raw = file_url.split('/')[-1]
     # make hash to get file name
     file_name = str(abs(hash(file_name_raw)))
     logger.info(f'Downloading {file_name}')
-    try:
-        r = requests.get(file_url, allow_redirects=True, proxies=proxies, timeout=None)
-        file_type =  urllib.request.urlopen(file_url).info()['content-type']
-        if file_type == 'application/pdf':
-            file_name += '.pdf'
-        elif file_type == 'application/msword':
-            file_name += '.doc'
-        elif file_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-            file_name += '.docx'
-        elif file_type == 'application/zip':
-            file_name += '.zip'
-        elif file_type == 'text/html':
-            file_name += '.html'
-        else: 
-            print(file_type)
-            with open('unknown_file_types.txt', 'a') as f:
+    for attempt in range(max_retries):
+        try:
+            response = requests.get(file_url, proxies=proxies, timeout=None)
+            response.raise_for_status()  # Check for HTTP errors
+
+            if response.status_code == 200:
+                # Request was successful, process the response
+                file_type =  urllib.request.urlopen(file_url).info()['content-type']
+                if file_type == 'application/pdf':
+                    file_name += '.pdf'
+                elif file_type == 'application/msword':
+                    file_name += '.doc'
+                elif file_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml':
+                    file_name += '.docx'
+                elif file_type == 'application/zip':
+                    file_name += '.zip'
+                elif file_type == 'text/html':
+                    file_name += '.html'
+                elif file_type == 'application/pdf;type=pdfx':
+                    file_name += '.pdf'
+                elif file_type == 'application/pdf;type=pdfa1b':
+                    file_name += '.pdf'
+                elif file_type == 'application/pdf;type=pdfa1a':
+                    file_name += '.pdf'
+                elif file_type == 'application/xhtml+xml':
+                    file_name += '.html'
+                elif file_type == 'text/html;charset=UTF-8':
+                    file_name += '.html'
+                else: 
+                    print(file_type)
+                    with open('unknown_file_types.txt', 'a') as f:
+                        f.write(file_url + '\n')
+                        return None
+                file_path = os.path.join(saving_dir, file_name)
+                open(file_path, 'wb').write(r.content)
+                logger.info(f'Downloaded {file_path}')
+                break
+
+        except requests.exceptions.HTTPError as e:
+            if response.status_code == 503 and attempt < max_retries - 1:
+                print(f"Received HTTP 503 error. Retrying in 5 seconds... (Attempt {attempt + 1}/{max_retries})")
+                sleep(5)  # Wait before retrying
+            else:
+                print(f"Request failed with status code {response.status_code}: {e}")
+                logger.error(f'Error downloading {file_url} with error {e}')
+                with open('download_errors.txt', 'a') as f:
+                    f.write(file_url + '\n')
+                break  # Exit the retry loop
+
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed: {e}")
+            logger.error(f'Error downloading {file_url} with error {e}')
+            with open('download_errors.txt', 'a') as f:
                 f.write(file_url + '\n')
-                return None
-        file_path = os.path.join(saving_dir, file_name)
-        open(file_path, 'wb').write(r.content)
-        logger.info(f'Downloaded {file_path}')
-    except Exception as e:
-        logger.error(f'Error downloading {file_url} with error {e}')
-        with open('download_errors.txt', 'a') as f:
-            f.write(file_url + '\n')
+            break  
 
 
 def main():
